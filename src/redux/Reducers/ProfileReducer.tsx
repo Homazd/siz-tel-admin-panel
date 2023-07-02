bfdd59c4bf8420a9eca9bde9a4422998569ce003
homazd <homa.zohdi@hotmail.com>
2023-07-02 17:52:59 +0330

    feat: add redux rtk

---
+++ src/redux/Reducers/ProfileReducer.tsx
@@ -0,0 +1,16 @@
+import {
+  FETCH_PROFILES_BEGIN,
+  FETCH_PROFILES_SUCCESS,
+  FETCH_PROFILES_FAILURE,
+} from "../Types/ProfileTypes";
+
+const initialState = {
+  items: [],
+  loading: false,
+  error: null,
+};
+export const profileReducer = () => {
+    return (
+        <div>Homa</div>
+    )
+}
