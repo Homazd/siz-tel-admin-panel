5fc9bad52ae36008d97fabb99c9b9ce820a35f4a
homazd <homa.zohdi@hotmail.com>
2023-07-10 15:50:53 +0330

    feat(profile): add profile form and icons

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import * as React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "./services/api";
 
