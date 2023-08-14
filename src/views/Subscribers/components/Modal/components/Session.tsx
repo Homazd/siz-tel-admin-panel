7f731d2a9d871a59e9a238703729e6f005fd0a49
homazd <homa.zohdi@hotmail.com>
2023-08-14 10:35:59 +0330

    feat: add env file and configure tsconfig

--- src/views/Subscribers/components/Modal/components/Session.tsx
+++ src/views/Subscribers/components/Modal/components/Session.tsx
@@ -1,51 +1,6 @@
 import { Divider, Select } from "@mantine/core";
 import ReusableInput from "../../../../../components/Input";
-const qciItems = [
-  {
-    id: 1,
-    title: "1",
-  },
-  {
-    id: 2,
-    title: "2",
-  },
-  {
-    id: 3,
-    title: "3",
-  },
-  {
-    id: 4,
-    title: "4",
-  },
-  {
-    id: 5,
-    title: "65",
-  },
-  {
-    id: 6,
-    title: "66",
-  },
-  {
-    id: 7,
-    title: "67",
-  },
-  {
-    id: 8,
-    title: "75",
-  },
-  {
-    id: 9,
-    title: "71",
-  },
-  {
-    id: 10,
-    title: "72",
-  },
-  {
-    id: 11,
-    title: "73",
-  },
-];
+import qciItems from ""
 const Session = () => {
   return (
     <div className="mt-10">
@@ -71,17 +26,27 @@ const Session = () => {
           className="ml-6 w-[100px]"
         />
       </div>
-      <div className="flex">
-       <Select
-       label="5QI/QCI"
-       required
-       clearable
-       data={qciItems.map((item) => (
-        {value: {item.title}, label:{item.title}}
-       )
-      )}
+
+      <Select
+        label="5QI/QCI"
+        className="mt-3 w-[425px]"
+        required
+        clearable
+        data={qciItems.map((option) => ({
+          value: option.title,
+          label: option.title,
+        }))}
       />
-      </div>
+      <Select
+        label="ARP Priority Level (1-15)"
+        className="mt-3 w-[425px]"
+        required
+        clearable
+        data={qciItems.map((option) => ({
+          value: option.title,
+          label: option.title,
+        }))}
+      />
     </div>
   );
 };
