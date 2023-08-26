0091b55b6ff0843f74d1dde3fe2657712b7798e3
homazd <homa.zohdi@hotmail.com>
2023-08-26 14:09:38 +0330

    feat: refactor imsi input

--- src/views/Subscribers/components/IMSIInput.tsx
+++ src/views/Subscribers/components/IMSIInput.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent } from 'react';
-
+import { useState } from "react";
+import { ChangeEvent, FormEvent } from "react";
+// Mantine
 import {
   TextInput,
   TextInputProps,
@@ -8,10 +9,7 @@ import {
 } from "@mantine/core";
 import { IconSearch, IconArrowRight, IconArrowLeft } from "@tabler/icons-react";
 import styled from "@emotion/styled";
-import { useState } from "react";
 import { useGetSubscribersQuery } from "../../../services/subscribers";
-
-
 
 const StyledInput = styled(TextInput)`
   & .mantine-TextInput-wrapper {
@@ -25,7 +23,8 @@ export interface SubscriberType {
 }
 
 function IMSIInput(props: TextInputProps) {
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>("");
+  const [isTyping, setIsTyping] = useState(false);
   const theme = useMantineTheme();
 
   const {
@@ -34,41 +33,51 @@ function IMSIInput(props: TextInputProps) {
     isSuccess,
     isError,
     error,
-  } = useGetSubscribersQuery({ imsi : value});
+  } = useGetSubscribersQuery(
+    { imsi: value },
+    {
+      skip: isTyping,
+    }
+  );
 
-  const handleOnInput = (event : ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
+  const handleOnInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setIsTyping(true);
     setValue(event.target.value);
-    console.log("value is:", value)
-    console.log(Subscribers, isLoading, isSuccess, isError, error)
+    // console.log(Subscribers, isLoading, isSuccess, isError, error);
+  };
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    setIsTyping(false);
+    event.preventDefault();
+    // handle form submit
   }
-
   return (
-    <StyledInput
-      icon={<IconSearch size="1.1rem" stroke={1.5} />}
-      radius="xl"
-      size="lg"
-      rightSection={
-        <ActionIcon
-          size={22}
-          radius="xl"
-          color={theme.primaryColor}
-          variant="filled"
-          maw={320}
-        >
-          {theme.dir === "ltr" ? (
-            <IconArrowRight size="1.1rem" stroke={1.5} />
-          ) : (
-            <IconArrowLeft size="1.1rem" stroke={1.5} />
-          )}
-        </ActionIcon>
-      }
-      placeholder="IMSI"
-      rightSectionWidth={22}
-      value={value}
-      onChange={handleOnInput}
-      {...props}
-    />
+    <form onSubmit={handleSubmit}>
+      <StyledInput
+        icon={<IconSearch size="1.1rem" stroke={1.5} />}
+        radius="xl"
+        size="lg"
+        rightSection={
+          <ActionIcon
+            size={22}
+            radius="xl"
+            color={theme.primaryColor}
+            variant="filled"
+            maw={320}
+          >
+            {theme.dir === "ltr" ? (
+              <IconArrowRight size="1.1rem" stroke={1.5} />
+            ) : (
+              <IconArrowLeft size="1.1rem" stroke={1.5} />
+            )}
+          </ActionIcon>
+        }
+        placeholder="IMSI"
+        rightSectionWidth={22}
+        value={value}
+        onChange={handleOnInput}
+        {...props}
+      />
+    </form>
   );
 }
 export default IMSIInput;
