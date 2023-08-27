48b36f8d5007f1c701907d2c8a8d12d6d1efd596
homazd <homa.zohdi@hotmail.com>
2023-08-27 13:40:06 +0330

    feat: add subscriber info modal

--- src/views/Subscribers/components/IMSIInput.tsx
+++ src/views/Subscribers/components/IMSIInput.tsx
@@ -6,10 +6,14 @@ import {
   TextInputProps,
   ActionIcon,
   useMantineTheme,
+  Box,
+  Modal,
+  Group,
 } from "@mantine/core";
 import { IconSearch, IconArrowRight, IconArrowLeft } from "@tabler/icons-react";
 import styled from "@emotion/styled";
 import { useGetSubscribersQuery } from "../../../services/subscribers";
+import { ModalsProvider } from "@mantine/modals";
 
 const StyledInput = styled(TextInput)`
   & .mantine-TextInput-wrapper {
@@ -25,6 +29,7 @@ export interface SubscriberType {
 function IMSIInput(props: TextInputProps) {
   const [value, setValue] = useState<string>("");
   const [isTyping, setIsTyping] = useState(false);
+  const [opened, setOpened] = useState(false);
   const theme = useMantineTheme();
 
   const {
@@ -45,7 +50,6 @@ function IMSIInput(props: TextInputProps) {
     setValue(event.target.value);
     console.log(Subscriber, isLoading, isSuccess, isError, error);
     console.log(Subscriber.imsi);
-    
   };
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     setIsTyping(false);
@@ -54,37 +58,74 @@ function IMSIInput(props: TextInputProps) {
   }
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <StyledInput
-          icon={<IconSearch size="1.1rem" stroke={1.5} />}
-          radius="xl"
-          size="lg"
-          rightSection={
-            <ActionIcon
-              size={22}
-              radius="xl"
-              color={theme.primaryColor}
-              variant="filled"
-              maw={320}
-            >
-              {theme.dir === "ltr" ? (
-                <IconArrowRight size="1.1rem" stroke={1.5} />
-              ) : (
-                <IconArrowLeft size="1.1rem" stroke={1.5} />
-              )}
-            </ActionIcon>
-          }
-          placeholder="IMSI"
-          rightSectionWidth={22}
-          value={value}
-          onChange={handleOnInput}
-          {...props}
-        />
-      </form>
-      {isLoading && <div>Loading...</div>}
-      {isError && <div>Error Fetching Subscriber data</div>}
-      {isSuccess && <div>{Subscriber.imsi}</div>}
- 
+      <ModalsProvider>
+        <form onSubmit={handleSubmit}>
+          <StyledInput
+            icon={<IconSearch size="1.1rem" stroke={1.5} />}
+            radius="xl"
+            size="lg"
+            rightSection={
+              <ActionIcon
+                size={22}
+                radius="xl"
+                color={theme.primaryColor}
+                variant="filled"
+                maw={320}
+              >
+                {theme.dir === "ltr" ? (
+                  <IconArrowRight size="1.1rem" stroke={1.5} />
+                ) : (
+                  <IconArrowLeft size="1.1rem" stroke={1.5} />
+                )}
+              </ActionIcon>
+            }
+            placeholder="IMSI"
+            rightSectionWidth={22}
+            value={value}
+            onChange={handleOnInput}
+            {...props}
+          />
+        </form>
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>Error Fetching Subscriber data</div>}
+        {isSuccess && (
+          <>
+            <Modal opened={opened} onClose={() => setOpened(false)} title="IMSI">
+              Hi Babe
+            </Modal>
+            <Group position="center" onClick={() => setOpened(true) }>
+              <Box
+                component="a"
+                target="_blank"
+                className="w-[300px] h-6 grid content-center"
+                sx={(theme) => ({
+                  // display: 'block',
+                  backgroundColor:
+                    theme.colorScheme === "dark"
+                      ? theme.colors.dark[6]
+                      : theme.colors.gray[3],
+                  color:
+                    theme.colorScheme === "dark"
+                      ? theme.colors.blue[4]
+                      : theme.colors.blue[8],
+                  padding: theme.spacing.xl,
+                  borderRadius: theme.radius.md,
+                  cursor: "pointer",
+
+                  "&:hover": {
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.colors.dark[5]
+                        : theme.colors.gray[5],
+                  },
+                })}
+              >
+                <div>IMSI: {Subscriber.imsi} </div>
+              </Box>
+            </Group>
+          </>
+        )}
+      </ModalsProvider>
     </>
   );
 }
